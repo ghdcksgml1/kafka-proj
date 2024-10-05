fd629096f58ff193222a1e8286833176248d558b
ghdcksgml1 <ghdcksgml2@naver.com>
2024-10-05 10:22:27 +0000

    section3:Producer 예제 구현

--- producers/src/main/java/com/example/kafka/SimpleProducer.java
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -1,7 +1,33 @@
 package com.example.kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 public class SimpleProducer {
-    KafkaProducer
+    public static void main(String[] args) {
+        String topicName = "simple-topic";
+        // KafkaProducer configuration setting
+        // null, "hello world"
+
+        Properties props = new Properties();
+        // bootstrap.servers, key.serializer.class, value.serializer.class
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 객체 생성
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // ProducerRecord 객체 생성
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "id-001", "아리갓또");
+
+        // KafkaProducer message send
+        kafkaProducer.send(producerRecord);
+        kafkaProducer.flush();
+        kafkaProducer.close();
+    }
 }
